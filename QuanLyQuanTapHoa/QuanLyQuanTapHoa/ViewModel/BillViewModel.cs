cb319ef0c53c253189bb13381576e55b5e7b047d
vominhtuan1 <19522487@gm.uit.edu.vn>
2021-10-14 10:37:43 +0700

    Giao diện của BillWindow

---
+++ QuanLyQuanTapHoa/QuanLyQuanTapHoa/ViewModel/BillViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyQuanTapHoa.ViewModel
+{
+    class BillViewModel : BaseViewModel
+    {
+        public ICommand OpenDetailCommand { get; set; }
+        public BillViewModel()
+        {
+            OpenDetailCommand = new RelayCommand<MainWindow>((p) => { return true; }, (p) => { OpenDetail(); });
+        }
+        public void OpenDetail()
+        {
+            BillDetailWindow billDetailWindow = new BillDetailWindow();
+            billDetailWindow.ShowDialog();
+        }
+    }
+}

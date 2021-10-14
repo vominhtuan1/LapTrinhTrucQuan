b93e8bba65123d4bb9baf53d4427b9ec6ec8dff8
Tú Võ Xuân <19522461@ms.uit.edu.vn>
2021-10-14 11:12:39 +0700

    Giao diện màn hình Setting

---
+++ QuanLyQuanTapHoa/QuanLyQuanTapHoa/ViewModel/SettingViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyQuanTapHoa.ViewModel
+{
+    class SettingViewModel : BaseViewModel
+    {
+        public ICommand OpenAddProduct { get; set; }
+        public ICommand OpenEditProduct { get; set; }
+
+        public SettingViewModel()
+        {
+            OpenAddProduct = new RelayCommand<SettingViewModel>((p) => { return true; }, (p) => { OpenAddProductWD(); });
+            OpenEditProduct = new RelayCommand<SettingViewModel>((p) => { return true; }, (p) => { OpenEditProductWD(); });
+        }
+
+        public void OpenAddProductWD()
+        {
+            AddProductWindow addProductWindow = new AddProductWindow();
+            addProductWindow.ShowDialog();
+        }
+
+        public void OpenEditProductWD()
+        {
+            EditProductWindow editProductWindow = new EditProductWindow();
+            editProductWindow.ShowDialog();
+        }
+    }
+}

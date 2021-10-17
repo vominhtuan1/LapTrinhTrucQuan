046481dfd1cb8e94d70746ed00a4bf5187127385
19520289 <19520289@gm.uit.edu.vn>
2021-10-17 22:31:51 +0700

    discout

---
+++ QuanLyQuanTapHoa/QuanLyQuanTapHoa/ViewModel/DiscountViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyQuanTapHoa.ViewModel
+{
+    class DiscountViewModel : BaseViewModel
+    {
+        public ICommand OpenAddDiscount { get; set; }
+
+        public DiscountViewModel()
+        {
+            OpenAddDiscount = new RelayCommand<SettingViewModel>((p) => { return true; }, (p) => { OpenAddDiscountWD(); });
+        }
+
+        public void OpenAddDiscountWD()
+        {
+            AddDiscountWindow addDiscountWindow = new AddDiscountWindow();
+            addDiscountWindow.ShowDialog();
+        }
+    }
+}

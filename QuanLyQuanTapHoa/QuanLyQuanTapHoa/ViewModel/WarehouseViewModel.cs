6876607600c10257feba081ec582da04dbadfd79
Tuan3738 <19522489@gm.uit.edu.vn>
2021-10-18 17:28:15 +0700

    Add Warehouse module

---
+++ QuanLyQuanTapHoa/QuanLyQuanTapHoa/ViewModel/WarehouseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+namespace QuanLyQuanTapHoa.ViewModel
+{
+    class WarehouseViewModel:BaseViewModel
+    {
+
+        public ICommand OpenImportProduct { get; set; }
+
+        public WarehouseViewModel()
+        {
+            OpenImportProduct = new RelayCommand<WarehouseViewModel>((p) => { return true; }, (p) => { OpenImportProductWD(); });
+        }
+
+        public void OpenImportProductWD()
+        {
+            ImportProductWindow ImportProduct = new ImportProductWindow();
+            ImportProduct.ShowDialog();
+        }
+    }
+}
